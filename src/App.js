47e43af109095b8d5963766bd58a7a58e6b35161
Brennan <brennanosunkwo14@gmail.com>
2023-11-27 01:15:32 -0500

    completed markdown-previewer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import { marked } from "marked";
 
+// a header (H1 size), a sub header (H2 size),
+// a link, inline code, a code block, a list item, a blockquote, an image, and bolded text
 function App() {
+const[new_text, setNewText] = useState(`
+# H1
+## H2
+[title](https://www.example.com)
+\`code\`
+\`\`\`
+{
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+}
+\`\`\`
+
+- First item
+- Second item
+- Third item
+
+> blockquote
+
+![alt text](image.jpg)
+
+**bold text**
+
+`);
+
+marked.setOptions({
+  breaks: true
+})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea id="editor" onChange={(event) => {setNewText(event.target.value);}}
+        value={new_text}>
+
+        </textarea>
+      <div id="preview" dangerouslySetInnerHTML={{__html: marked(new_text),}}></div>
+
     </div>
   );
 }
